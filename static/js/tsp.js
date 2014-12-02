9689241b94bc9b69ce4b3940ca6ba68aa9cbab7a
Florie Coggins <florie.coggins@gmail.com>
2014-12-02 11:14:15 -0800

    added background information to static sections

--- static/js/tsp.js
+++ static/js/tsp.js
@@ -25,6 +25,7 @@ $(function() {
     $( "#results" ).draggable();
   });
 
+//scrolling for web pages
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
